254cda41e86814c67d46e5a83048c5dfc0b3c059
Luke Forehand <lukeforehand@gmail.com>
2020-02-06 13:28:04 -0500

    fixed image background

--- ReadingScreen.js
+++ ReadingScreen.js
@@ -26,14 +26,14 @@ export default class ReadingScreen extends React.Component {
 
     return(
       <SafeAreaView>
-        <ScrollView>
-          <ImageBackground
+        <ImageBackground
             resizeMode='stretch'
             style={{width: '100%', height: '100%'}}
             source={{ uri: 'https://spacestationplaza.com/images/space.jpg' }}>
+          <ScrollView>
             <Text style={[style.text, { marginLeft: '4%', marginRight: '4%'}]}>{'\n' + this.props.navigation.dangerouslyGetParent().getParam('reading').join('\n\n')}</Text>
-          </ImageBackground>
-        </ScrollView>
+          </ScrollView>
+        </ImageBackground>
       </SafeAreaView>
     );
   }
