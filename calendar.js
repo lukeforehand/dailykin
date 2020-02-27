0c15a630c1e8750deeb1a65f92e5252314992cc9
Luke Forehand <lukeforehand@gmail.com>
2020-02-26 23:40:29 -0500

    added moonphase

--- calendar.js
+++ calendar.js
@@ -1,27 +1,18 @@
-
 //blue -> grey
 //yellow -> gold
 //red -> gold
 //white -> grey
 
-glyphs = [
-  'Dali',
-  'Seli',
-  'Gamma',
-  'Kali',
-  'Alpha',
-  'Limi',
-  'Sillio'
-];
+glyphs = ['Dali', 'Seli', 'Gamma', 'Kali', 'Alpha', 'Limi', 'Sillio'];
 
 colors = {
-  'Dali': 'yellow',
-  'Seli': 'red',
-  'Gamma': 'white',
-  'Kali': 'blue',
-  'Alpha': 'yellow',
-  'Limi': 'red',
-  'Sillio': 'white'
+  Dali: 'yellow',
+  Seli: 'red',
+  Gamma: 'white',
+  Kali: 'blue',
+  Alpha: 'yellow',
+  Limi: 'red',
+  Sillio: 'white',
 };
 
 tones = [
@@ -37,7 +28,7 @@ tones = [
   'Spectral',
   'Planetary',
   'Crystal',
-  'Cosmic'
+  'Cosmic',
 ];
 
 tribes = [
@@ -60,7 +51,7 @@ tribes = [
   'Red Earth',
   'White Mirror',
   'Blue Storm',
-  'Yellow Sun'
+  'Yellow Sun',
 ];
 
 START_DATE = new Date(Date.UTC(1900, 0, 1, 12, 0, 0));
@@ -93,21 +84,28 @@ function leapDaysBetween(start, end) {
   return leapDays;
 }
 
-function calculateDay(date, dayOffset=0) {
+function calculateDay(date, dayOffset = 0) {
   // set date to UTC at noon
-  date = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 12, 0, 0));
+  date = new Date(
+    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 12, 0, 0),
+  );
   date.setDate(date.getDate() + dayOffset);
 
   // calculate days since first known kin day
-  days = Math.round((date.getTime() - START_DATE.getTime()) / (24 * 60 * 60 * 1000));
+  days = Math.round(
+    (date.getTime() - START_DATE.getTime()) / (24 * 60 * 60 * 1000),
+  );
 
   // calculate leap days which must be subtracted from the days since first known kin
   leapDays = leapDaysBetween(START_DATE.getFullYear(), date.getFullYear());
-  if (date.getTime() < new Date(Date.UTC(date.getFullYear(), 1, 29, 12, 0, 0)).getTime()) {
+  if (
+    date.getTime() <
+    new Date(Date.UTC(date.getFullYear(), 1, 29, 12, 0, 0)).getTime()
+  ) {
     // date is before Feb 29, so the year's leap day hasn't happened yet
     leapDays = leapDays - 1;
   }
-  days = (days - leapDays);
+  days = days - leapDays;
 
   tribe = (days + START_TRIBE) % 20;
   tone = (days + START_TONE) % 13;
@@ -115,15 +113,18 @@ function calculateDay(date, dayOffset=0) {
 
   // calculate the years since first known kin day
   years = date.getFullYear() - START_DATE.getFullYear();
-  if (date.getTime() > new Date(Date.UTC(date.getFullYear(), 6, 25, 12, 0, 0)).getTime()) {
+  if (
+    date.getTime() >
+    new Date(Date.UTC(date.getFullYear(), 6, 25, 12, 0, 0)).getTime()
+  ) {
     // date is after July 25, the start of a new galactic year
     years = years + 1;
   }
 
   // calculate how many moons this year has seen up to the date
-  moon = (Math.floor(((days + START_MOONDAY) - years) / 28) + START_MOON) % 13;
+  moon = (Math.floor((days + START_MOONDAY - years) / 28) + START_MOON) % 13;
   // calculate the day of the current moon
-  moonDay = ((days + START_MOONDAY) - years) % 28;
+  moonDay = (days + START_MOONDAY - years) % 28;
   glyph = moonDay % 7;
 
   return {
@@ -133,16 +134,12 @@ function calculateDay(date, dayOffset=0) {
     tribe: tribe + 1 + ' ' + tribes[tribe],
     moon: moon + 1,
     moonDay: moonDay + 1,
-    glyph: glyph + 1 + ' ' + glyphs[glyph]
+    glyph: glyph + 1 + ' ' + glyphs[glyph],
   };
-
 }
-
-
-
 
 //date = new Date();
 date = new Date(2498, 9, 22);
-for (var i=0; i < 60; i++) {
+for (var i = 0; i < 60; i++) {
   console.info(calculateDay(date, i));
 }
--- moonphase.js
+++ moonphase.js
@@ -1,34 +1,46 @@
 // 1980 January 0.0 in JDN
-epoch = 2444238.5;
+var epoch = 2444238.5;
 
 // Ecliptic longitude of the Sun at epoch 1980.0
-ecliptic_longitude_epoch = 278.833540;
+var ecliptic_longitude_epoch = 278.83354;
 
 // Ecliptic longitude of the Sun at perigee
-ecliptic_longitude_perigee = 282.596403;
+var ecliptic_longitude_perigee = 282.596403;
 
 // Eccentricity of Earth's orbit
-eccentricity = 0.016718;
+var eccentricity = 0.016718;
 
 // Moon's mean longitude at the epoch
-moon_mean_longitude_epoch = 64.975464;
+var moon_mean_longitude_epoch = 64.975464;
 
 // Mean longitude of the perigee at the epoch
-moon_mean_perigee_epoch = 349.383063;
+var moon_mean_perigee_epoch = 349.383063;
 
 // Synodic month (new Moon to new Moon), in days
-synodic_month = 29.53058868;
+var synodic_month = 29.53058868;
+
+var phase_strings = {
+  0.05: 'new moon',
+  0.20: 'waxing crescent',
+  0.30: 'first quarter',
+  0.45: 'waxing gibbous',
+  0.55: 'full moon',
+  0.70: 'waning gibbous',
+  0.80: 'last quarter',
+  0.95: 'waning crescent',
+  1.05: 'new moon',
+};
 
 function juliandate(date) {
-  return (date.getTime() / 86400000) + 2440587.5;
+  return date.getTime() / 86400000 + 2440587.5;
 }
 
 function kepler(m, ecc) {
-  epsilon = 1e-6;
-  m = torad(m);
-  e = m;
+  var epsilon = 1e-6;
+  var m = torad(m);
+  var e = m;
   while (true) {
-    delta = e - ecc * Math.sin(e) - m;
+    var delta = e - ecc * Math.sin(e) - m;
     e = e - delta / (1.0 - ecc * Math.cos(e));
     if (Math.abs(delta) <= epsilon) {
       break;
@@ -38,116 +50,102 @@ function kepler(m, ecc) {
 }
 
 function fixangle(a) {
-  return a - 360.0 * Math.floor(a/360.0);
+  return a - 360.0 * Math.floor(a / 360.0);
 }
 
 function torad(d) {
-  return d * Math.PI / 180.0;
+  return (d * Math.PI) / 180.0;
 }
 
 function todeg(r) {
-  return r * 180.0 / Math.PI;
+  return (r * 180.0) / Math.PI;
 }
 
-PRECISION = 0.05;
-NEW = 0 / 4.0;
-FIRST = 1 / 4.0;
-FULL = 2 / 4.0;
-LAST = 3 / 4.0;
-NEXTNEW = 4 / 4.0;
+function phase(date, dayOffset = 0) {
 
-function phase_string(p) {
-  phase_strings = (
-    (0.05, 'new'),
-    (0.2,  'waxing crescent'),
-    (0.3,  'first quarter'),
-    (0.45, 'waxing gibbous'),
-    (0.55, 'full'),
-    (0.7,  'waning gibbous'),
-    (0.8,  'last quarter'),
-    (0.95, 'waning crescent'),
-    (1.05, 'new')
-  );
-  //i = bisect.bisect([a[0] for a in phase_strings], p);
-  //return phase_strings[i][1];
-  return 'yo';
-}
+  date = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 12, 0, 0));
+  date.setDate(date.getDate() + dayOffset);
 
-function phase(date) {
-  day = juliandate(date) - epoch;
+  var day = juliandate(date) - epoch;
 
   // Mean anomaly of the Sun
-  N = fixangle((360/365.2422) * day);
+  var N = fixangle((360 / 365.2422) * day);
 
   // Convert from perigee coordinates to epoch 1980
-  M = fixangle(N + ecliptic_longitude_epoch - ecliptic_longitude_perigee);
-  
+  var M = fixangle(N + ecliptic_longitude_epoch - ecliptic_longitude_perigee);
+
   // Solve Kepler's equation
-  Ec = kepler(M, eccentricity);
+  var Ec = kepler(M, eccentricity);
   Ec = Math.sqrt((1 + eccentricity) / (1 - eccentricity)) * Math.tan(Ec / 2.0);
-  
+
   // True anomaly
   Ec = 2 * todeg(Math.atan(Ec));
-  
+
   // Suns's geometric ecliptic longuitude
-  lambda_sun = fixangle(Ec + ecliptic_longitude_perigee);
-  
-  // Orbital distance factor
-  F = ((1 + eccentricity * Math.cos(torad(Ec))) / (1 - eccentricity**2));
-  
-  // Distance to Sun in km
-  //sun_dist = sun_smaxis / F;
-  //sun_angular_diameter = F * sun_angular_size_smaxis;
+  var lambda_sun = fixangle(Ec + ecliptic_longitude_perigee);
 
   // Calculation of the Moon's position
 
   // Moon's mean longitude
-  moon_longitude = fixangle(13.1763966 * day + moon_mean_longitude_epoch);
+  var moon_longitude = fixangle(13.1763966 * day + moon_mean_longitude_epoch);
 
   // Moon's mean anomaly
-  MM = fixangle(moon_longitude - 0.1114041 * day - moon_mean_perigee_epoch);
+  var MM = fixangle(moon_longitude - 0.1114041 * day - moon_mean_perigee_epoch);
 
   // Moon's ascending node mean longitude
-  evection = 1.2739 * Math.sin(torad(2*(moon_longitude - lambda_sun) - MM));
+  var evection =
+    1.2739 * Math.sin(torad(2 * (moon_longitude - lambda_sun) - MM));
 
   // Annual equation
-  annual_eq = 0.1858 * Math.sin(torad(M));
+  var annual_eq = 0.1858 * Math.sin(torad(M));
 
   // Correction term
-  A3 = 0.37 * Math.sin(torad(M));
+  var A3 = 0.37 * Math.sin(torad(M));
 
-  MmP = MM + evection - annual_eq - A3;
+  var MmP = MM + evection - annual_eq - A3;
 
   // Correction for the equation of the centre
-  mEc = 6.2886 * Math.sin(torad(MmP));
+  var mEc = 6.2886 * Math.sin(torad(MmP));
 
   // Another correction term
-  A4 = 0.214 * Math.sin(torad(2 * MmP));
+  var A4 = 0.214 * Math.sin(torad(2 * MmP));
 
   // Corrected longitude
-  lP = moon_longitude + evection + mEc - annual_eq + A4;
+  var lP = moon_longitude + evection + mEc - annual_eq + A4;
 
   // Variation
-  variation = 0.6583 * Math.sin(torad(2*(lP - lambda_sun)));
+  var variation = 0.6583 * Math.sin(torad(2 * (lP - lambda_sun)));
 
   // True longitude
-  lPP = lP + variation;
+  var lPP = lP + variation;
 
   // Age of the Moon, in degrees
-  moon_age = lPP - lambda_sun;
+  var moon_age = lPP - lambda_sun;
 
   // Phase of the Moon
-  moon_phase = (1 - Math.cos(torad(moon_age))) / 2.0;
+  var moon_phase = (1 - Math.cos(torad(moon_age))) / 2.0;
 
-  return {
-    phase: fixangle(moon_age) / 360.0,
-    illuminated: moon_phase,
-    age: synodic_month * fixangle(moon_age) / 360.0
+  var phase = fixangle(moon_age) / 360.0;
+
+  var phase_name;
+  for (var key in phase_strings) {
+    if (phase < key) {
+      phase_name = phase_strings[key];
+      break;
+    }
   }
 
+  return {
+    date: date,
+    age: (synodic_month * fixangle(moon_age)) / 360.0,
+    phase: phase,
+    name: phase_name,
+    illuminated: moon_phase,
+  };
 }
 
 date = new Date();
-phase_result = phase(date);
-phase_text = phase_string(phase_result);
-console.info('The moon is ' + phase_text + ', ' + (phase_result.illuminated * 100).toFixed(1) + '% illuminated, ' + phase_result.age.toFixed(1) + ' days old.');
+for (var i = 0; i < 30; i++) {
+  console.info(phase(date, i));
+}
+
